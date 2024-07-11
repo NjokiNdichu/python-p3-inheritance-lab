f970a746f49bcb69ad4bdc7f5101c13690c842b4
Njoki Ndichu <rejoicenjoki0@gmail.com>
2024-07-11 12:09:15 +0300

    inheritance

--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,38 @@ from user import User
 
 import random
 
+
+
+
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
+    
+    @property
+    def knowledge(self):
+        return self._knowledge
+    
+    @knowledge.setter
+    def knowledge(self, knowledge):
+        if len(knowledge) > 0:
+            self._knowledge = knowledge
+        else:
+            raise ValueError("knowledge list cannot be empty")
+        
+
+
     def teach(self):
-        pass
+        return random.choice(self._knowledge)
